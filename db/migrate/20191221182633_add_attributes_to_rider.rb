0ad4ee2d963b4f686071e22fcfccf0d6497652c3
Tayler Johnson <johnsontayler@me.com>
2019-12-21 15:35:58 -0300

    db migrate

--- db/migrate/20191221182633_add_attributes_to_rider.rb
+++ db/migrate/20191221182633_add_attributes_to_rider.rb
@@ -2,7 +2,6 @@ class AddAttributesToRider < ActiveRecord::Migration[5.2]
   def change
     add_column :riders, :first_name, :string
     add_column :riders, :last_name, :string
-    add_column :riders, :email, :string
     add_column :riders, :photo, :string
     add_column :riders, :address, :string
   end
