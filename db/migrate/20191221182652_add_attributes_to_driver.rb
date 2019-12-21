f19ed52148ffa86881f6f9b125d960fcf59c9ace
Tayler Johnson <johnsontayler@me.com>
2019-12-21 15:34:02 -0300

    add attributes to rider driver

---
+++ db/migrate/20191221182652_add_attributes_to_driver.rb
@@ -0,0 +1,9 @@
+class AddAttributesToDriver < ActiveRecord::Migration[5.2]
+  def change
+    add_column :drivers, :first_name, :string
+    add_column :drivers, :last_name, :string
+    add_column :drivers, :email, :string
+    add_column :drivers, :photo, :string
+    add_column :drivers, :address, :string
+  end
+end
