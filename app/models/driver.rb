4ec42855d515c6bb20642d4db1470288933199ac
Tayler Johnson <johnsontayler@me.com>
2019-12-21 15:38:40 -0300

    rider driver validations

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,9 @@ class Driver < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
 end
