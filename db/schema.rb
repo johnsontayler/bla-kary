4556278336b96ea945f0e2923f069f05c205dc92
Tayler Johnson <johnsontayler@me.com>
2019-12-22 13:16:03 -0300

    dashboards controller

--- db/schema.rb
+++ db/schema.rb
@@ -60,18 +60,6 @@ ActiveRecord::Schema.define(version: 2019_12_22_155414) do
     t.index ["weekly_listing_id"], name: "index_rides_on_weekly_listing_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   create_table "weekly_listings", force: :cascade do |t|
     t.date "beginning_of_week"
     t.date "end_of_week"
