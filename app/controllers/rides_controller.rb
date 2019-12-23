c67c3d58a73d7be6cffdbfc74c3762adf84e0185
Tayler Johnson <johnsontayler@me.com>
2019-12-23 12:21:22 -0300

    ride simple form

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -3,9 +3,14 @@ class RidesController < ApplicationController
   end
 
   def new
+    @ride = Ride.new
   end
 
   def create
+    @ride = Ride.new(ride_params)
+    @ride.rider = current_user
+    @ride.save
+    redirect_to dashboards_path
   end
 
   def edit
@@ -15,5 +20,11 @@ class RidesController < ApplicationController
     @ride = Ride.find(params[:id])
     @ride.update!(weekly_schedule: false)
     redirect_to dashboards_path
+  end
+
+  private
+
+  def ride_params
+    params.require(:ride).permit(:date, :time, :pick_up, :drop_off, :price)
   end
 end
