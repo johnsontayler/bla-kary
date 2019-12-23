80265860ee1931ab29bdb28b3212be50c1f74f96
Tayler Johnson <johnsontayler@me.com>
2019-12-23 16:46:41 -0300

    dashboard  method to limit rides to current week

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,25 @@
 class DashboardsController < ApplicationController
   def index
-    rider = Rider.find(current_user.id)
     @ride = Ride.new
-    @weekly_schedule = rider.rides.where(weekly_schedule: true)
+    @rider = Rider.find(current_user.id)
+
+    only_rides_of_current_week
+    @weekly_schedule = @rider.rides.where(weekly_schedule: true).sort_by{|d| d[:time]}
+    debugger
   end
 
   def update
+  end
+
+  private 
+
+  def only_rides_of_current_week
+    today = Date.today
+    current_week = [*today.at_beginning_of_week..today.at_end_of_week]
+    days_from_current_week = current_week.map {|date| date.strftime("%A, %m/%d/%Y")}
+
+    @rider.rides.each do |ride|
+      ride_path(ride) if !days_from_current_week.include?(ride.date)
+    end
   end
 end
