c29a2953f7cc6bb0409c8823f7fd74aa258f6284
Tayler Johnson <tayler.johnson@buck.tv>
2020-01-24 12:45:54 -0800

    accept and deny contract bids

--- app/controllers/riders_controller.rb
+++ app/controllers/riders_controller.rb
@@ -1,5 +1,6 @@
 class RidersController < ApplicationController
   def index
     @riders = Rider.where(schedule_submitted: true).sort_by{|d| d[:time]}
+    @driver = Driver.find(current_user.id)
   end
 end
