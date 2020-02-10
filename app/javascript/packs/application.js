55bb558468f3820f3b8ee78d324c4369c96eb90c
Tayler Johnson <tayler.johnson@buck.tv>
2020-02-10 14:58:21 -0800

    btn denied

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,15 @@
 import "bootstrap";
+
+const btnsAccept = document.querySelectorAll(".btn-accept");
+const btnsDeny = document.querySelectorAll(".btn-deny");
+
+if (btnsDeny) {
+  btnsDeny.forEach((btn, index) => {
+    btn.addEventListener("click", event => {
+      event.currentTarget.style =
+        "background-color: #8f0d0d; color: white; border: 1px solid #8f0d0d;";
+      event.currentTarget.value = "Denied";
+      btnsAccept[index].style.display = "none";
+    });
+  });
+}
